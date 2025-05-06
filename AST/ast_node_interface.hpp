2f346d2c3101bc71dc953d5005a84ea3287cb666
Julio Cesar Vasquez Garcia <tu.email@example.com>
2025-05-06 13:33:46 -0400

    actualizacion ast

--- AST/ast_node_interface.hpp
+++ AST/ast_node_interface.hpp
@@ -21,7 +21,7 @@ public:
     // Método para representación en string 
     virtual std::string to_string() const noexcept = 0;
 
-    // Método para resolución de nombres en el análisis semántico
+    // Método para verificacion en el análisis semántico
     virtual bool resolve_names(SymbolTable& table) noexcept = 0;
 };
 // Alias útil para representar un cuerpo de instrucciones
